d656832ac0deb5b294a0c0fd540d3e66da27bdfb
ValeriiBielik <bielik.my@gmail.com>
2022-10-18 20:09:36 +0300

    [feature] - refactor main navigation, fix menu displaying

--- app/src/main/java/com/bielik/progresstracker/feature/main/MainActivity.kt
+++ app/src/main/java/com/bielik/progresstracker/feature/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bielik.progresstracker.feature.main
 
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.bielik.progresstracker.R
 import com.bielik.progresstracker.base.BaseSplashActivity
 import com.bielik.progresstracker.databinding.ActivityMainBinding
@@ -24,13 +23,6 @@ class MainActivity : BaseSplashActivity<ActivityMainBinding, MainViewModel>() {
     private fun setupNavigationGraph(shouldLaunchOnBoarding: Boolean) {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerMain) as NavHostFragment
         navHostFragment.navController.setGraph(if (shouldLaunchOnBoarding) R.navigation.nav_on_boarding else R.navigation.nav_main_container)
-    }
-
-    // todo remove
-    private fun setUpBottomMenu() = withBinding {
-        bottomNavView.itemIconTintList = null
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerMain) as NavHostFragment
-        bottomNavView.setupWithNavController(navHostFragment.navController)
     }
 
     override fun attachBinding() = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/bielik/progresstracker/feature/main/MainContainerFragment.kt
+++ app/src/main/java/com/bielik/progresstracker/feature/main/MainContainerFragment.kt
@@ -3,6 +3,9 @@ package com.bielik.progresstracker.feature.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.bielik.progresstracker.R
 import com.bielik.progresstracker.base.BaseBindingFragment
 import com.bielik.progresstracker.base.EmptyViewModel
 import com.bielik.progresstracker.databinding.FragmentMainContainerBinding
@@ -14,7 +17,13 @@ class MainContainerFragment : BaseBindingFragment<FragmentMainContainerBinding, 
     override val viewModel by viewModels<EmptyViewModel>()
 
     override fun initUI() {
+        setUpBottomMenu()
+    }
 
+    private fun setUpBottomMenu() = withBinding {
+        bottomNavView.itemIconTintList = null
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerSubMain) as NavHostFragment
+        bottomNavView.setupWithNavController(navHostFragment.navController)
     }
 
     override fun attachBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean) =
