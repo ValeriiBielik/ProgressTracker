73ed832763e66d05e1ac3d0f3f99ff4886472e0d
ValeriiBielik <42238034+ValeriiBielik@users.noreply.github.com>
2022-10-22 13:57:44 +0300

    [feature] - implement userName displaying on ProfileFragment (#3)
    Co-authored-by: MaximBielik <maxim.bielik2007@gmail.com>

--- app/src/main/java/com/bielik/progresstracker/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/bielik/progresstracker/feature/profile/ProfileFragment.kt
@@ -1,6 +1,27 @@
 package com.bielik.progresstracker.feature.profile
 
-import androidx.fragment.app.Fragment
-import com.bielik.progresstracker.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.bielik.progresstracker.base.BaseBindingFragment
+import com.bielik.progresstracker.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class ProfileFragment : Fragment(R.layout.fragment_profile)
+@AndroidEntryPoint
+class ProfileFragment : BaseBindingFragment<FragmentProfileBinding, ProfileViewModel>() {
+    override val viewModel by viewModels<ProfileViewModel>()
+
+    override fun initUI() {
+        subscribe()
+    }
+
+    private fun subscribe() {
+        viewModel.setupFlow.observeWhenResumed {
+            binding?.tvUserName?.text = it
+        }
+    }
+
+    override fun attachBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean) =
+        FragmentProfileBinding.inflate(inflater, container, false)
+
+}
---
+++ app/src/main/java/com/bielik/progresstracker/feature/profile/ProfileViewModel.kt
@@ -0,0 +1,19 @@
+package com.bielik.progresstracker.feature.profile
+
+import com.bielik.progresstracker.base.BaseViewModel
+import com.bielik.progresstracker.repository.PreferencesRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    preferencesRepository: PreferencesRepository
+) : BaseViewModel() {
+    val setupFlow: SharedFlow<String> = MutableSharedFlow(replay = 1)
+
+    init {
+        setupFlow.emitViewModelScope(preferencesRepository.userName)
+    }
+}
