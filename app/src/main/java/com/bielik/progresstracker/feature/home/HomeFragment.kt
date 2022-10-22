4faab8d0f6784c827ce03216f5fc8cb0aa273031
ValeriiBielik <42238034+ValeriiBielik@users.noreply.github.com>
2022-10-22 13:58:09 +0300

    Feature/implement appbar on home fragment (#4)
    * [feature] - implement userName displaying on ProfileFragment
    
    * [feature] - implement userName displaying on ProfileFragment
    
    Co-authored-by: MaximBielik <maxim.bielik2007@gmail.com>

--- app/src/main/java/com/bielik/progresstracker/feature/home/HomeFragment.kt
+++ app/src/main/java/com/bielik/progresstracker/feature/home/HomeFragment.kt
@@ -1,6 +1,26 @@
 package com.bielik.progresstracker.feature.home
 
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.bielik.progresstracker.R
+import com.bielik.progresstracker.base.BaseBindingFragment
+import com.bielik.progresstracker.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+@AndroidEntryPoint
+class HomeFragment : BaseBindingFragment<FragmentHomeBinding, HomeViewModel>() {
+    override val viewModel by viewModels<HomeViewModel>()
+    override fun initUI() {
+        subscribe()
+    }
+
+    private fun subscribe(){
+        viewModel.setupFlow.observeWhenResumed {
+            binding?.tvGreetingUser?.text = getString(R.string.format_user_greeting, it)
+        }
+    }
+
+    override fun attachBinding(inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean) =
+        FragmentHomeBinding.inflate(inflater, container, false)
+}
